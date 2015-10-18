952856758e73b828a0e55706ca0f78fa49fc0aa2
Piji You <you_peijie@yahoo.com>
2015-10-18 13:44:46 +0800

    bug fixed for cue_converter
    转换成功的时候才覆盖原来的cue咯...

--- cue_converter.py
+++ cue_converter.py
@@ -6,8 +6,9 @@ for parent, dirnames, filenames in os.walk(rootdir):
 	for file in filenames:
 		fullname = os.path.join(parent, file)
 		if fullname.endswith('.cue'):
-			print(fullname)
 			os.system('cp "%s" "%s.temp"' %(fullname, fullname))
 			if (os.system('iconv -f GBK -t UTF-8 "%s">"%s.temp"' %(fullname, fullname)) != 0):
-				u = input()
-			os.system('mv "%s.temp" "%s"' %(fullname, fullname))
+				print(fullname)
+				#u = input()
+			else:
+				os.system('mv "%s.temp" "%s"' %(fullname, fullname))
